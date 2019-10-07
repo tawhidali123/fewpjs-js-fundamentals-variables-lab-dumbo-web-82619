be469bab523d49152beb40f348640c5addab895f
Tawhid Ali <tawhidali126@gmail.com>
2019-10-07 13:02:17 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+let companyName = "Scuber";
+const companyName = "Scuber";
+
+function mostProfitableNeighborhood(){
+    let place = "chelsea";
+    return "place";
+}
+
+function companyCeo(){
+    const ceo = "Lauren Hart";
+    return ceo;
+}
+
+
